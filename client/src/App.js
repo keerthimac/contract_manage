04c2e1245fef355dd7da7c9c87c3f2b2d31590f1
keeth-office <keerthi.mac@gmail.com>
2022-04-07 13:20:34 +0530

    form initiated

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,96 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { useRef, useState } from "react";
+import FromInput from "./components/FormInput";
 
 function App() {
-  return <div className="App"></div>;
+  const [values, setValues] = useState({
+    username: "",
+    email: "",
+    birthday: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const inputs = [
+    {
+      id: 1,
+      name: "username",
+      type: "text",
+      placeholder: "Username",
+      errorMessage:
+        "Username should be at 3-16 characters and shouldn't include any special character.",
+      label: "Username",
+      pattern: "^[A-Za-z0-9]{3,16}$",
+      required: true,
+    },
+    {
+      id: 2,
+      name: "email",
+      type: "text",
+      placeholder: "Email",
+      errorMessage: "Email should be valid.",
+      label: "Email",
+      pattern: "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,4}$",
+      required: true,
+    },
+    {
+      id: 3,
+      name: "birthday",
+      type: "date",
+      placeholder: "Birthday",
+      label: "Birthday",
+    },
+    {
+      id: 4,
+      name: "password",
+      type: "password",
+      placeholder: "Password",
+      errorMessage:
+        "Username should be at least 6 characters long and at least 1 letter, 1 capital letter.",
+      label: "Password",
+      required: true,
+    },
+    {
+      id: 5,
+      name: "confirmPassword",
+      type: "password",
+      placeholder: "Confirm Password",
+      errorMessage: "password and confirm password should be the same.",
+      label: "Confirm Password",
+      pattern: values.password,
+      required: true,
+    },
+  ];
+
+  const onFromSubmit = (e) => {
+    e.preventDefault();
+    const data = new FormData(e.target);
+    console.log(Object.fromEntries(data.entries()));
+  };
+
+  const onChange = (e) => {
+    setValues({
+      ...values,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  return (
+    <div className="app">
+      <form onSubmit={onFromSubmit}>
+        <h1>Register</h1>
+        {inputs.map((input) => (
+          <FromInput
+            key={input.id}
+            {...input}
+            value={values[input.name]}
+            onChange={onChange}
+          />
+        ))}
+        <button>Submit</button>
+      </form>
+    </div>
+  );
 }
 
 export default App;
---
+++ client/src/components/FormInput.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import "./formInput.css";
+
+function FormInput(props) {
+  const { id, label, errorMessage, onChange, ...inputProps } = props;
+  const [focused, setFocused] = useState(false);
+
+  const handleFocus = (e) => {
+    setFocused(true);
+  };
+
+  return (
+    <div className='formInput'>
+      <label>{label}</label>
+      <input
+        {...inputProps}
+        onChange={onChange}
+        onBlur={handleFocus}
+        focused={focused.toString()}
+      />
+      <span>{errorMessage}</span>
+    </div>
+  );
+}
+
+export default FormInput;
