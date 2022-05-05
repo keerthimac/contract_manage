dc9494ea5e09eae89b5a8ef0580475ba4d74c8bd
keeth-office <keerthi.mac@gmail.com>
2022-05-05 12:53:12 +0530

    added top bar not working

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import Topbar from "./components/topbar/Topbar";
 
 import SubInfo from "./components/SubInfo/SubInfo";
 function App() {
   return (
     <div className="app">
+      <Topbar />
       <SubInfo />
     </div>
   );
---
+++ client/src/components/topbar/Topbar.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import "./topbar.css";
+import SettingsIcon from "@material-ui/icons/Settings";
+import LanguageIcon from '@mui/icons-material/Language';
+import NotificationsNoneIcon from "@material-ui/icons/NotificationsNone";
+
+function Topbar() {
+  return (
+    <div className='topbar'>
+      <div className='topbarWrapper'>
+        <div className='topLeft'>
+          <span className='logo'>React Admin</span>
+        </div>
+        <div className='topRight'>
+          <div className='topbarIconsContainer'>
+            <NotificationsNoneIcon className='topIcon' />
+            <span className='topIconBadge'>2</span>
+          </div>
+          <div className='topbarIconsContainer'>
+            <LanguageIcon className='topIcon' />
+            <span className='topIconBadge'>2</span>
+          </div>
+          <div className='topbarIconsContainer'>
+            <SettingsIcon className='topIcon' />
+            <span className='topIconBadge'>2</span>
+          </div>
+          <img
+            src='https://images.pexels.com/photos/1526814/pexels-photo-1526814.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500'
+            alt=''
+            className='avatar'
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Topbar;
