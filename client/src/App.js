b6225b647dcf76d1e3e00c0d079ec577c0c61863
keeth-office <keerthi.mac@gmail.com>
2022-06-23 17:03:18 +0530

    corrected

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,8 @@ import Topbar from "./components/topbar/Topbar";
 import SubInfo from "./components/SubInfo/SubInfo";
 function App() {
   return (
-<<<<<<< HEAD
     <div className="app">
       <Topbar />
-=======
-    <div className='app'>
->>>>>>> 78f4e3d58bc3d95fac841d3ebbfc8a55b5c78b8c
       <SubInfo />
     </div>
   );
